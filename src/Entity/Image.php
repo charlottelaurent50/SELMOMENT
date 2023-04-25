42f0bc2e43ad72c440d7a31883f4a26b91242b1c
charlottelaurent50 <charlottelau50@gmail.com>
2023-04-25 21:51:20 +0200

    ajout entite image

---
+++ src/Entity/Image.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 500)]
+    private ?string $chemin = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getChemin(): ?string
+    {
+        return $this->chemin;
+    }
+
+    public function setChemin(string $chemin): self
+    {
+        $this->chemin = $chemin;
+
+        return $this;
+    }
+}
