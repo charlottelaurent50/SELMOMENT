51199ec91f03fa22c40f6c097321c68a0a9208af
charlottelaurent50 <charlottelau50@gmail.com>
2023-04-25 21:52:32 +0200

    ajout entite evenement

---
+++ src/Entity/Evenement.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EvenementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EvenementRepository::class)]
+class Evenement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $titre = null;
+
+    #[ORM\Column(length: 500, nullable: true)]
+    private ?string $descriptif = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $date_ev = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescriptif(): ?string
+    {
+        return $this->descriptif;
+    }
+
+    public function setDescriptif(?string $descriptif): self
+    {
+        $this->descriptif = $descriptif;
+
+        return $this;
+    }
+
+    public function getDateEv(): ?\DateTimeInterface
+    {
+        return $this->date_ev;
+    }
+
+    public function setDateEv(?\DateTimeInterface $date_ev): self
+    {
+        $this->date_ev = $date_ev;
+
+        return $this;
+    }
+}
